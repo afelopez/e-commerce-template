a7f0685ac9621db3eaa901a26ffecbea32978073
Andrés López <dev.andres.lopez@gmail.com>
2025-01-03 15:32:13 -0500

    [Add] Cards components

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,18 @@
-import { Layout } from '../../Components/Layout'
+import { Layout } from "../../Components/Layout"
+import { Card } from "../../Components/Card"
 
 function Home() {
     return(
-        <Layout children={ <h1> Home </h1> } />
+        <Layout children={ 
+            <>  
+                <Card 
+                    title={"Card Title"}
+                    price={100} 
+                    tag={"description"}
+                    srcImage={"https://images.pexels.com/photos/29986994/pexels-photo-29986994/free-photo-of-intricate-blue-abstract-pattern-on-green-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+                />
+            </>
+        } />
     )
 }
 
